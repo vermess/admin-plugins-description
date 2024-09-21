5bc824e9e99e72ce9bd3e6e0470fcf6c6078d535
Michał <michal.robak@gmail.com>
2024-09-21 20:48:06 +0200

    Remove public part

--- includes/class-admin-plugins-description.php
+++ includes/class-admin-plugins-description.php
@@ -79,7 +79,6 @@ class Admin_Plugins_Description
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 	}
 
 	/**
@@ -118,11 +117,7 @@ class Admin_Plugins_Description
 		 */
 		require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-admin-plugins-description-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-admin-plugins-description-public.php';
+
 
 		$this->loader = new Admin_Plugins_Description_Loader();
 	}
@@ -162,22 +157,6 @@ class Admin_Plugins_Description
 		$this->loader->add_filter('plugin_row_meta', $plugin_admin, 'add_description_link', 9999, 2);
 		$this->loader->add_action('wp_ajax_send_ajax_request', $plugin_admin, 'handle_ajax_request');
 		$this->loader->add_action('wp_ajax_remove_description', $plugin_admin, 'handle_remove_description');
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks()
-	{
-
-		$plugin_public = new Admin_Plugins_Description_Public($this->get_plugin_name(), $this->get_version());
-
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
-		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
 	}
 
 	/**
